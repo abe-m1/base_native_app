5119a32c1a8195d9c22ce88c5d9bda2e1f2accf6
abe <abestbet1@gmail.com>
2017-02-07 13:32:45 -0800

    create test component to test navigiation

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class EmployeeList extends Component {
+    render(){
+        return(
+            <View>
+                <Text>hello</Text>
+                 <Text>hello</Text>
+                  <Text>hello</Text>
+                   <Text>hello</Text>
+            </View>
+        )
+    }
+}
+
+
+export default EmployeeList
