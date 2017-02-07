98f9fe9ae6962e36e53645a02f282e4c5bf8a2e7
abe <abestbet1@gmail.com>
2017-02-07 10:33:09 -0800

    set up redux thunk as middleware

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { View, Text } from 'react-native';
-import { createStore } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers'
 import LoginForm from './components/LoginForm'
 
 class App extends Component {
     render(){
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+        //apply middleware is the third argument
+        //2nd argument is for any initial state
+        //3rd argument is for store enhancers 
         return(
-            <Provider store={createStore(reducers)}>
+            <Provider store={store}>
                 <View>
                     <LoginForm />
                 </View>
