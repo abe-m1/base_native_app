e134b40365873947f8d70f15caada86454ac2b7a
abe <abestbet1@gmail.com>
2017-02-08 11:23:18 -0800

    link up ticketForm reducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import AuthReducer from './AuthReducer'
+import TicketFormReducer from './TicketFormReducer'
 
 export default combineReducers({
-    auth: AuthReducer
+    auth: AuthReducer,
+    ticketForm: TicketFormReducer
 })
