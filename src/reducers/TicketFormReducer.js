04ef23e01e8a7f2ea90b1505180bf3f3a48e1dd2
abe <abestbet1@gmail.com>
2017-02-07 20:42:42 -0800

    wire up ticketForm reducer

--- src/reducers/TicketFormReducer.js
+++ src/reducers/TicketFormReducer.js
@@ -1,11 +1,20 @@
 import { TICKET_UPDATE } from '../actions/types'
 
-const INITIAL_STATE = {}
+const INITIAL_STATE = {title: '', category: '', status: ''}
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type){
-        
+        case TICKET_UPDATE:
+            //action.payload === {prop: 'name', value: 'jane'}
+           return { ...state, [action.payload.prop]: action.payload.value}
         default:
             return state
     }
 }
+
+
+//square brackets are not an array , it is key interpolation
+
+//in es5 you would need to do the following:
+//const newState = {}
+//newState[action.payload.prop] = action.payload.value
