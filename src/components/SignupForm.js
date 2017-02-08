ef7dacc3a2fda22b5ab3553db7a5d89a5bf7f490
abe <abestbet1@gmail.com>
2017-02-08 10:40:53 -0800

    create basic component for sign up

---
+++ src/components/SignupForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Text } from 'react-native'
+import { Card, CardSection, Input,  Button, Spinner } from './common';
+
+
+class SignupForm extends Component{
+    
+
+    render(){
+        return(
+           <Text> Sign up form </Text>
+        )
+    }
+}
+
+
+export default SignupForm
