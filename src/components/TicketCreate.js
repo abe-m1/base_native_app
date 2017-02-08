0ee82348d8bc3226d49fc88edb283eae9167a028
abe <abestbet1@gmail.com>
2017-02-07 18:26:51 -0800

    create ticketCreate component

---
+++ src/components/TicketCreate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class TicketCreate extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Ticket Form</Text>
+            </View>
+        )
+    }
+}
+
+export default TicketCreate
