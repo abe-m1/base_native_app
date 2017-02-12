3f32f87f48acc4f61ced6c658a00508cab386d3a
abe <abestbet1@gmail.com>
2017-02-12 13:35:52 -0800

    GET_TICKETS reducer logic

--- src/reducers/TicketFormReducer.js
+++ src/reducers/TicketFormReducer.js
@@ -1,4 +1,4 @@
-import { TICKET_UPDATE } from '../actions/types'
+import { TICKET_UPDATE, GET_TICKETS } from '../actions/types'
 
 const INITIAL_STATE = {title: '', category: '', status: ''}
 
@@ -7,6 +7,9 @@ export default (state = INITIAL_STATE, action) => {
         case TICKET_UPDATE:
             //action.payload === {prop: 'name', value: 'jane'}
            return { ...state, [action.payload.prop]: action.payload.value}
+
+        case GET_TICKETS:
+            return { ...state, tickets: action.payload}
         default:
             return state
     }
