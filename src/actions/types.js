3c7ae712b0cb799782e855d5a3d89465cf879591
abe <abestbet1@gmail.com>
2017-02-12 13:24:06 -0800

    create GET_TICKETS type

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,6 +3,7 @@ export const PASSWORD_CHANGED = 'password_changed'
 export const LOGIN_USER_SUCCESS = 'login_user_success'
 export const LOGIN_USER_FAIL = 'login_user_fail'
 export const LOGIN_USER = 'login_user'
+export const GET_TICKETS = 'get_tickets'
 
 export const TICKET_UPDATE = 'ticket_update'
 
