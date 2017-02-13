1900d4daf528d57e213d8bfa56b53e4cc5d2db8b
abe <abestbet1@gmail.com>
2017-02-13 10:59:16 -0800

    create list view in TicketList

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ListView } from 'react-native'
 import { connect } from 'react-redux'
 import { getTickets } from '../actions';
 
 class TicketList extends Component {
     componentWillMount(){
         this.props.getTickets()
+        this.createDataSource(this.props)
     }
+
+    componentWillReceiveProps(nextProps){
+        this.createDataSource(nextProps)
+    }
+
+    createDataSource({ tickets}){
+         //destructure this.props
+         const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) =>r1 !== r2
+        })
+
+        this.dataSource = ds.cloneWithRows(tickets)
+         
+    }
+
+    renderRow(ticket){
+        return <ListItem ticket={ticket} />
+       
+    }
+    
     render(){
         console.log(this.props.tickets)
         return(
